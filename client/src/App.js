81d9986892af78a1b86144e87eaae6113ba7946a
Jasmine <jasminetumulak@gmail.com>
2025-02-20 09:04:29 +0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
