4e7d7b6941a5ddfb15affe5fa8db45879eafabce
Jasmine <jasminetumulak@gmail.com>
2025-04-08 09:47:56 +0400

    Added registeruser Server API

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import express from "express";
 import UserModel from "./Models/UserModel.js";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -11,10 +12,32 @@ app.use(cors());
 const connectString =
   "mongodb+srv://admin:admin12345@postitcluster.vcbpdnh.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
 
-mongoose.connect(connectString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(connectString);
+//API Routes
+
+app.post("/registerUser", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const user = new UserModel({
+      name: name,
+      email: email,
+      password: hashedpassword,
+    });
+
+    await user.save();
+    res.send({ user: user, msg: "Added." });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+app.post("/login", async (req, res) => {});
+
+app.put("/updateProfile", async (req, res) => {});
 
 app.listen(3001, () => {
   console.log("You are connected thank you");
